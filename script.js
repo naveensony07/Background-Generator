b5145f975facd5cb29efdb52155be32b000e92ba
Naveen Sony <116912198+naveensony07@users.noreply.github.com>
2023-10-01 13:39:19 +0530

    Backgroung Generator

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('generate').addEventListener('click', function() {
+    const color1 = document.getElementById('color1').value;
+    const color2 = document.getElementById('color2').value;
+    
+    document.body.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
+});
